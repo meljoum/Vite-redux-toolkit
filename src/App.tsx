cf349ec5fd3233ba7f12cf69f45c61aca09ecf9d
Elmehdiflyer <elmehdi.meljoum@flyer.be>
2023-06-13 15:44:43 +0100

    first commit with initial Reduxjs/tollkit

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+//import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { useAppDispatch, useAppSelector } from './app/hooks'
+import { amountAdded, decremented, incremented } from './featuers/counter/counterSlice'
+import './App.css'
+
+function App() {
+  //*const [count, setCount] = useState(0)
+
+  //*const handleChange = () => setCount((count) => count + 1)
+
+
+  // we get the state from redux
+  const count =  useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch();
+
+  function handleIncrementCounter() {
+    dispatch(incremented());
+  }
+
+  function handleDecrementedCounter() {
+    dispatch(decremented());
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleChange(e: any) {
+    e.target.value
+  }
+
+  function handleAmountAdded() {
+    dispatch(amountAdded(3));
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleIncrementCounter}> + </button>
+        <div className="input-txt h-8 w-1/2 my-auto mx-2">
+          <input type="number" onChange={handleChange} name="" id="" />
+        </div>
+        <p>The count is : {count}</p>
+        <button onClick={handleDecrementedCounter}> - </button>
+        <button onClick={handleAmountAdded}>Amount Added</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/featuers/counter/counterSlice.ts
@@ -0,0 +1,35 @@
+// DUCKS Pattern
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+
+
+interface CounterState {
+    value: number;
+}
+
+const initialState: CounterState = {
+    value: 0,
+}
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        incremented(state) {
+            // it's okay to do this because immer makes it immutable
+            state.value++;
+        },
+        decremented(state) {
+            state.value--;
+        },
+        amountAdded(state, action:PayloadAction<number>) {
+            state.value += action.payload;
+        }
+    }
+
+});
+
+
+export const { incremented, decremented, amountAdded } = counterSlice.actions;
+
+export default counterSlice.reducer;
