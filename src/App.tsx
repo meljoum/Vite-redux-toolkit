6e7787ba5cb331243a30f2386fd471934397a885
Elmehdiflyer <elmehdi.meljoum@flyer.be>
2023-06-19 14:36:03 +0100

    fix bug responsive

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ function App() {
 
 
   return (
-    <>
+    <div className="container">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -134,7 +134,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
